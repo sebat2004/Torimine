6a585d89978e9b2e85c165007275b0dd42faa3fd
sebat2004 <sebastiantorresola3@gmail.com>
2023-06-21 22:59:43 -0700

    Initilized project and added serverside functionality

---
+++ server/server.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+const morgan = require("morgan");
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+// Logging
+app.use(morgan('dev'))
+
+// Attach body to request
+app.use(express.json());
+
+// Get specific employee
+app.get('/api/employees/:id', (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      id: req.params.id,
+      salary: 100000
+    },
+    message: `Got Employee ${req.params.id}}`
+  });
+});
+
+// Get all employees
+app.get('/api/employees/', (req, res) => {
+  res.json({
+    status: "success",
+    message: "Got All Employees"
+  });
+});
+
+// Create new employee
+app.post('/api/employees/', (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.status(201).json({
+    status: "success",
+    message: `Created new employee under id #${body.id}`
+  });
+});
+
+// Update employee
+app.patch('/api/employees/:id', (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.status(200).json({
+    status: "success",
+    message: `Updated employee #${req.params.id}`
+  });
+});
+
+// Delete employee
+app.delete('/api/employees/:id', (req, res) => {
+  res.status(204)
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`
+    );
+});
