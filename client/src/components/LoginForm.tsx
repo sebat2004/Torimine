96eabb7e86ca44a734a8238640f9a54c550efcc7
Seba <sebastiantorresola3@gmail.com>
2023-07-07 20:30:00 -0700

    Added new tab functionality to login and register forms

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,21 +1,30 @@
 import { Link } from "react-router-dom"
 
 const LoginForm = () => {
+    const openInNewTab = (url: string) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+        }
+
   return (
     <>
         <div className="flex flex-col items-center justify-center h-[90vh]">
-            <div className="py-6 px-10 drop-shadow-lg flex rounded-lg flex-col bg-base-200 lg:w-[40vw] gap-6">
+            <div className="py-7 px-10 drop-shadow-lg flex rounded-lg bg-[#e6e8e8] flex-col lg:w-[40vw] gap-6">
                 <h1 className="text-2xl font-semibold">Login</h1>
                 <form className="flex flex-col gap-4">
                     <div className="flex flex-col gap-1">
                         <label className="text-left">Username/E-mail</label>
-                        <input className="input" type="text" placeholder="Username or e-mail" />
+                        <input className="input bg-[#e0e1e0] border-opacity-10" type="text" placeholder="Username or e-mail" />
                     </div>
                     <div className="flex flex-col gap-1">
                         <label className="text-left">Password</label>
-                        <input className="input" type="password" placeholder="Password" />
+                        <input className="input bg-[#e0e1e0] border-opacity-10" type="password" placeholder="Password" />
                     </div>
-                    <div className="flex flex-col self-center items-center pt-3 gap-4">
+                    <div className="flex flex-col self-center items-center pt-[13px] gap-5">
+                        <Link to="/login" className="text-md flex gap-[5px]">
+                            <h1 className="cursor-normal">Forgot Password?</h1>
+                            <h1 className="text-neutral font-bold" onClick={() => openInNewTab("http://localhost:5173/")}>Click here</h1>
+                        </Link>
                         <Link to="/workspace" className="shadow-lg btn-md btn btn-neutral w-[400px]">Login</Link>
                     </div>
                 </form>
--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,37 +1,45 @@
 import { Link } from "react-router-dom"
 
 const RegisterForm = () => {
-  return (
-    <>
-        <div className="flex flex-col items-center justify-center h-[90vh]">
-            <div className="py-6 px-10 drop-shadow-lg flex rounded-lg flex-col bg-base-200 lg:w-[40vw] gap-6">
-                <h1 className="text-2xl font-semibold">Create an account with us!</h1>
-                <form className="flex flex-col gap-4">
-                    <div className="flex flex-col gap-1">
-                        <label className="text-left">Username</label>
-                        <input className="input" type="text" placeholder="Username" />
-                    </div>
-                    <div className="flex flex-col gap-1">
-                        <label className="text-left">Email</label>
-                        <input className="input" type="email" placeholder="name@company.com" />
-                    </div>
-                    <div className="flex flex-col gap-1">
-                        <label className="text-left">Password</label>
-                        <input className="input" type="password" placeholder="Password" />
-                    </div>
-                    <div className="flex flex-col gap-1">
-                        <label className="text-left">Confirm Password</label>
-                        <input className="input" type="password" placeholder="Confirm Password" />
-                    </div>
-                    <div className="flex flex-col self-center items-center pt-3 gap-4">
-                        <Link to="/login" className="text-sm">Already have an account? Click to login.</Link>
-                        <Link to="/login" className="shadow-lg btn-md btn btn-neutral w-[400px]">Register</Link>
-                    </div>
-                </form>
+    const openInNewTab = (url: string) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+        }
+        
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center h-[90vh]">
+                <div className="py-7 px-10 drop-shadow-lg flex rounded-lg flex-col bg-[#e6e8e8] lg:w-[40vw] gap-6">
+                    <h1 className="text-2xl font-semibold">Create an account with us!</h1>
+                    <form className="flex flex-col gap-4">
+                        <div className="flex flex-col gap-1">
+                            <label className="text-left">Username</label>
+                            <input className="input bg-[#e0e1e0] border-opacity-10" type="text" placeholder="Username" />
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <label className="text-left">Email</label>
+                            <input className="input bg-[#e0e1e0] border-opacity-10" type="email" placeholder="name@company.com" />
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <label className="text-left">Password</label>
+                            <input className="input bg-[#e0e1e0] border-opacity-10" type="password" placeholder="Password" />
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <label className="text-left">Confirm Password</label>
+                            <input className="input bg-[#e0e1e0] border-opacity-10" type="password" placeholder="Confirm Password" />
+                        </div>
+                        <div className="flex flex-col self-center items-center pt-[13px] gap-5">
+                            <Link to="/login" className="text-md flex gap-[5px]">
+                                <h1 className="cursor-default">Already have an account?</h1>
+                                <h1 className="text-neutral font-bold" onClick={() => openInNewTab("http://localhost:5173/")}>Click here</h1>
+                            </Link>
+                            <Link to="/login" className="shadow-lg btn-md btn btn-neutral w-[400px]">Register</Link>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default RegisterForm
