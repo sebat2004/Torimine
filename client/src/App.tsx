5229a183288d84467f9059960d9d22d7063659ff
sebat2004 <sebastiantorresola3@gmail.com>
2023-07-23 20:43:31 -0700

    Added frontend auth checker and context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,11 +7,19 @@ import Workspace from '../routes/Workspace'
 import Schedule from '../routes/Schedule'
 import { createContext, useState, Dispatch, SetStateAction } from 'react'
 import Dashboard from '../routes/Dashboard'
-import Account from '../routes/Account'
+import People from '../routes/People'
 
 interface SidebarContext {
   isOpen: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+interface AuthContext {
+  user: {
+    username: string;
+    isAuthenticated: boolean;
+  };
+  setUser: Dispatch<SetStateAction<{username: string, isAuthenticated: boolean}>>;
 }
 
 export const SidebarContext = createContext<SidebarContext>({
@@ -19,24 +27,66 @@ export const SidebarContext = createContext<SidebarContext>({
   setOpen: () => {},
 });
 
+export const AuthContext = createContext<AuthContext>({
+  user: {
+    username : '',
+    isAuthenticated: false
+  },
+  setUser: () => {}
+})
+
 function App() {
+  const [user, setUser] = useState<{username: string, isAuthenticated: boolean}>({
+    username: '',
+    isAuthenticated: false
+  })
 
   const [isOpen, setOpen] = useState<boolean>(true);
+  const getAuthenticated = async () => {
+    if (!user.isAuthenticated) {
+      const response = await fetch('http://localhost:3000/api/authenticated', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+          })
+          .then(res => res.json())
+          .then(data => {
+            return data
+          }
+        )
+      if (response.status === 'success') {
+        setUser({
+          username: response.user.username,
+          isAuthenticated: true
+        })
+      } else {
+        setUser({
+          username: '',
+          isAuthenticated: false
+        })
+              }
+    }
+  }
+  getAuthenticated()
 
   return (
     <>
-      <SidebarContext.Provider value={{isOpen, setOpen}}>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/workspace" element={<Workspace />}>
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="schedule" element={<Schedule />} />
-              <Route path="account" element={<Account />} />
-            </Route>
-          </Routes>
-      </SidebarContext.Provider>
+      <AuthContext.Provider value={{user, setUser}}>
+        <SidebarContext.Provider value={{isOpen, setOpen}}>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/workspace" element={<Workspace />}>
+                <Route index element={<Dashboard />} />
+                <Route path="schedule" element={<Schedule />} />
+                <Route path="people" element={<People />} />
+              </Route>
+            </Routes>
+        </SidebarContext.Provider>
+      </AuthContext.Provider>
     </>
   )
 }
